bb03a66013f5412993c6a4ff1ef858e327f855bc
Melina Campana Rodriguez <mel.campana06@gmail.com>
2025-11-17 14:13:27 -0800

    Added text file, as well as code checking if  the file was open or not

---
+++ FinalProject/FinalProject.cpp
@@ -0,0 +1,47 @@
+// FinalProject.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <fstream>
+
+using namespace std;
+
+int main()
+{
+   cout << "Hello World!\n";
+
+   ofstream outputFile("bookInfo.txt"); //opens book information file
+
+   if (outputFile.is_open()) {    //checks if book information file is open
+	   cout << "Book Information file open" << endl;
+   }
+   else {
+	   cout << "File not opened" << endl;
+   }
+
+   if (outputFile.is_open()) { 
+	   outputFile << "Hello World" << endl;
+   }
+   else {
+	   cout << "File not opened" << endl;
+   }
+
+   outputFile.close(); //closes book information file
+
+
+
+
+}
+
+
+
+// Run program: Ctrl + F5 or Debug > Start Without Debugging menu
+// Debug program: F5 or Debug > Start Debugging menu
+
+// Tips for Getting Started: 
+//   1. Use the Solution Explorer window to add/manage files
+//   2. Use the Team Explorer window to connect to source control
+//   3. Use the Output window to see build output and other messages
+//   4. Use the Error List window to view errors
+//   5. Go to Project > Add New Item to create new code files, or Project > Add Existing Item to add existing code files to the project
+//   6. In the future, to open this project again, go to File > Open > Project and select the .sln file
